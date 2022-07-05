f5f488956c3ceb83ee878d0ab5e8fe2e5a52e67a
masum359160 <automation24@mis.prangroup.com>
2022-07-05 15:24:40 +0600

    Bootstrap, fontawesome, main functions page developed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Main Functions</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <div class="row text-center">
+                        <div class="col-sm-4">
+                            <a href="#">
+                                <h4>Managment</h4>
+                                <img width="50px" src="{{asset('images/management.svg')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="#">
+                                <h4>Cashier</h4>
+                                <img width="50px" src="{{asset('images/cashier.svg')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="#">
+                                <h4>Report</h4>
+                                <img width="50px" src="{{asset('images/report.svg')}}"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
